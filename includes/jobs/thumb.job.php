6cdac8dccc790df5fc6f2a940e6de58681261726
Foxel <foxel@quickfox.ru>
2016-06-03 00:10:30 +0700

    Fixed auto creating thumb cache dir

--- includes/jobs/thumb.job.php
+++ includes/jobs/thumb.job.php
@@ -21,33 +21,35 @@ Glob_Request('fid');
   elseif (!file_exists('files/'.$gfile['file'])) $err_logo='imgs/att_ferr.gif';
   elseif ($QF_User->level<$gfile['rights']) $err_logo='imgs/att_low.gif';
 
-  if(empty($err_logo)) {
+  if (empty($err_logo)) {
 
      $info=pathinfo($gfile['filename']);
      $mime=$mimes[strtolower($info['extension'])];
-     if ($mime['preview']) {         $gotfile = 'cache/thumbs/'.$gfile['file'].'.chc';
-         if (!file_exists($gotfile))
+     if ($mime['preview']) {
+         $gotfile = 'cache/thumbs/'.$gfile['file'].'.chc';
+         if (!is_dir('cache/thumbs/')) {
+             mkdir('cache/thumbs/', 0700, true);
+         }
+         if (!file_exists($gotfile)) {
              $gotfile = img_resize_to('files/'.$gfile['file'], $gotfile, $QF_Config['thumb_width'], $QF_Config['thumb_height']);
+         }
 
          if ($gotfile) {
+             $img_info = getimagesize($gotfile);
 
-	         $img_info = getimagesize($gotfile);
-
-	         $dfile = new DownLoadFile($gotfile, 'thumb-'.$gfile['filename'], $img_info['mime']);
-	         $dfile->out();
-         }
-         else
-         {
+             $dfile = new DownLoadFile($gotfile, 'thumb-'.$gfile['filename'], $img_info['mime']);
+             $dfile->out();
+         } else {
              $err_logo = 'imgs/att_ferr.gif';
              copy($err_logo, 'cache/thumbs/'.$gfile['file'].'.chc');
          }
-     }
-     else
+     } else {
          $err_logo = ($mime['icon']) ? 'imgs/mime/'.$mime['icon'] : 'imgs/mime/file.gif';
+     }
   }
 
   if ($err_logo)
-  {      $dfile = new DownLoadFile($err_logo);
+  {
+      $dfile = new DownLoadFile($err_logo);
       $dfile->out();
   }
-?>
